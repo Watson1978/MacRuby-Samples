a6cf12f62dde4099796d247d45ae788e7fb76bdd
Watson <watson1978@gmail.com>
2011-05-11 06:58:14 +0900

    first commit

---
+++ Translator/AppDelegate.rb
@@ -0,0 +1,60 @@
+#
+#  AppDelegate.rb
+#  Translator
+#
+#  Created by Watson on 11/05/11.
+#  Copyright 2011 __MyCompanyName__. All rights reserved.
+#
+
+# Google Translate API key
+API_KEY = 'AIzaSyDNLefOLDCAlDm5niZP_2LX7RIEdmnPwbM'
+
+class AppDelegate
+    require 'json'
+    require 'uri'
+    require 'open-uri'
+
+    attr_accessor :window, :text # outlet
+
+    # delegator, action
+    def execTranslate(sender)
+        source = 'en'
+        target = 'ja'
+        
+        str = getText
+        
+        if str.length > 0
+            open(URI.escape("https://www.googleapis.com/language/translate/v2?key=#{API_KEY}&source=#{source}&target=#{target}&q=#{str}")) {|f|
+                str = f.read
+                js = JSON.parse(str)
+                insertText(js['data']['translations'][0]['translatedText'])
+            }
+        end
+    end
+    
+    def textPaste(sender)
+        pboard = NSPasteboard.generalPasteboard
+        insertText(pboard.stringForType(NSStringPboardType))
+    end
+
+    def textCopy(sender)
+        pboard = NSPasteboard.generalPasteboard
+        pboard.declareTypes([NSStringPboardType], owner:nil);
+        pboard.setString(getText.to_s, forType:NSStringPboardType)
+    end
+    
+    def windowWillClose(sender)
+        exit
+    end
+
+    private
+
+    def getText
+        text.stringValue
+    end
+    
+    def insertText(str)
+        text.setStringValue(str.to_s)
+    end    
+end
+
