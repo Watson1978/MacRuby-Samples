c6857d15aedeab59979d510b9484258d3937f023
Watson <watson1978@gmail.com>
2011-09-10 18:23:28 +0900

    fix the indentation.

--- ButtonBar/ButtonBar/ButtonBar.h
+++ ButtonBar/ButtonBar/ButtonBar.h
@@ -2,9 +2,6 @@
 //  ButtonBar.h
 //  ButtonBar
 //
-//  Created by Watson _ on 11/09/10.
-//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
-//
 
 #import <Cocoa/Cocoa.h>
 
--- ButtonBar/ButtonBar/ButtonBar.m
+++ ButtonBar/ButtonBar/ButtonBar.m
@@ -9,10 +9,10 @@
 
 void drawLine(NSColor *color, CGFloat x, CGFloat y, CGFloat width, CGFloat height)
 {
-  NSRect rect = NSMakeRect(x, y, width, height);
-  NSFrameRectWithWidth(rect, 1);
-  [color set];
-	NSFrameRect(rect);
+    NSRect rect = NSMakeRect(x, y, width, height);
+    NSFrameRectWithWidth(rect, 1);
+    [color set];
+    NSFrameRect(rect);
 }
 
 static NSColor *topLineColor;
@@ -26,15 +26,15 @@ static NSGradient *gradient;
 
 + (void)initialize
 {
-	topLineColor       = [NSColor colorWithCalibratedWhite:(150.0f / 255.0f) alpha:1.0f];
-	bottomLineColor    = [NSColor colorWithCalibratedWhite:(150.0f / 255.0f) alpha:1.0f];
-  topColor           = [NSColor colorWithCalibratedWhite:(253.0f / 255.0f) alpha:1.0f];
-  middleTopColor     = [NSColor colorWithCalibratedWhite:(243.0f / 255.0f) alpha:1.0f];
-  middleBottomColor  = [NSColor colorWithCalibratedWhite:(230.0f / 255.0f) alpha:1.0f];
-	bottomColor        = [NSColor colorWithCalibratedWhite:(230.0f / 255.0f) alpha:1.0f];
-	resizeHandleColor  = [NSColor colorWithCalibratedWhite:(170.0f / 255.0f) alpha:0.598f];
-  
-	gradient = [[NSGradient alloc] initWithColorsAndLocations:
+    topLineColor       = [NSColor colorWithCalibratedWhite:(150.0f / 255.0f) alpha:1.0f];
+    bottomLineColor    = [NSColor colorWithCalibratedWhite:(150.0f / 255.0f) alpha:1.0f];
+    topColor           = [NSColor colorWithCalibratedWhite:(253.0f / 255.0f) alpha:1.0f];
+    middleTopColor     = [NSColor colorWithCalibratedWhite:(243.0f / 255.0f) alpha:1.0f];
+    middleBottomColor  = [NSColor colorWithCalibratedWhite:(230.0f / 255.0f) alpha:1.0f];
+    bottomColor        = [NSColor colorWithCalibratedWhite:(230.0f / 255.0f) alpha:1.0f];
+    resizeHandleColor  = [NSColor colorWithCalibratedWhite:(170.0f / 255.0f) alpha:0.598f];
+
+    gradient = [[NSGradient alloc] initWithColorsAndLocations:
                           topColor, 0.0f,
                           middleTopColor, 0.47f,
                           middleBottomColor, 0.49f,
@@ -48,27 +48,27 @@ static NSGradient *gradient;
     if (self) {
         // Initialization code here.
     }
-    
+
     return self;
 }
 
 - (void)drawRect:(NSRect)rect
 {
-	// Draw gradient
-	NSRect gradientRect;
-  gradientRect = NSMakeRect(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height - 1);
-  [gradient drawInRect:gradientRect angle:270];
+    // Draw gradient
+    NSRect gradientRect;
+    gradientRect = NSMakeRect(rect.origin.x, rect.origin.y, rect.size.width, rect.size.height - 1);
+    [gradient drawInRect:gradientRect angle:270];
 
-  // Draw Top line
-  drawLine(topLineColor, rect.origin.x, rect.size.height - 1, rect.size.width, rect.size.height - 1);
+    // Draw Top line
+    drawLine(topLineColor, rect.origin.x, rect.size.height - 1, rect.size.width, rect.size.height - 1);
 
-  // Draw Bottom line
-  drawLine(bottomLineColor, rect.origin.x, rect.origin.y, rect.size.width, rect.origin.y);
+    // Draw Bottom line
+    drawLine(bottomLineColor, rect.origin.x, rect.origin.y, rect.size.width, rect.origin.y);
 
-  // Draw Handle
-  for(int i=0; i < 3; i++) {
-    drawLine(resizeHandleColor, rect.size.width - 15 + i*4, rect.size.height/2 - 5, 1, 10);
-  }
+    // Draw Handle
+    for(int i=0; i < 3; i++) {
+	drawLine(resizeHandleColor, rect.size.width - 15 + i*4, rect.size.height/2 - 5, 1, 10);
+    }
 }
 @end
 
