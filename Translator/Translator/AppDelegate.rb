efbc2f57827fd322a397c3d9b4c4fb554a8305f2
Watson <watson1978@gmail.com>
2011-10-04 00:38:09 +0900

    Translator: support the Service Menu.

--- Translator/Translator/AppDelegate.rb
+++ Translator/Translator/AppDelegate.rb
@@ -10,51 +10,64 @@
 API_KEY = 'AIzaSyDNLefOLDCAlDm5niZP_2LX7RIEdmnPwbM'
 
 class AppDelegate
-    require 'json'
-    require 'uri'
-    require 'open-uri'
+  require 'json'
+  require 'uri'
+  require 'open-uri'
 
-    attr_accessor :window, :text # outlet
-
-    # delegator, action
-    def execTranslate(sender)
-        source = 'en'
-        target = 'ja'
-        
-        str = getText
-        
+  attr_accessor :window, :text # outlet
+  
+  def applicationDidFinishLaunching(a_notification)
+    NSApp.setServicesProvider(self)
+  end
+  
+  def doService(pboard, userData:userData, error:error)
+    window.makeKeyAndOrderFront(nil)
+    string = pboard.stringForType(NSStringPboardType)
+    
+    translate(string)
+  end
+  
+  # delegator, action
+  def execTranslate(sender)
+    string = getText
+    translate(string)
+  end
+  
+  def translate(str)
+    source = 'en'
+    target = 'ja'
         if str.length > 0
-            open(URI.escape("https://www.googleapis.com/language/translate/v2?key=#{API_KEY}&source=#{source}&target=#{target}&q=#{str}")) {|f|
-                str = f.read
-                js = JSON.parse(str)
-                insertText(js['data']['translations'][0]['translatedText'])
-            }
-        end
+      open(URI.escape("https://www.googleapis.com/language/translate/v2?key=#{API_KEY}&source=#{source}&target=#{target}&q=#{str}")) {|f|
+        str = f.read
+        js = JSON.parse(str)
+        insertText(js['data']['translations'][0]['translatedText'])
+      }
     end
-    
-    def textPaste(sender)
-        pboard = NSPasteboard.generalPasteboard
-        insertText(pboard.stringForType(NSStringPboardType))
-    end
+  end
 
-    def textCopy(sender)
-        pboard = NSPasteboard.generalPasteboard
-        pboard.declareTypes([NSStringPboardType], owner:nil);
-        pboard.setString(getText.to_s, forType:NSStringPboardType)
-    end
-    
-    def windowWillClose(sender)
-        exit
-    end
+  def textPaste(sender)
+    pboard = NSPasteboard.generalPasteboard
+    insertText(pboard.stringForType(NSStringPboardType))
+  end
 
-    private
+  def textCopy(sender)
+    pboard = NSPasteboard.generalPasteboard
+    pboard.declareTypes([NSStringPboardType], owner:nil);
+    pboard.setString(getText.to_s, forType:NSStringPboardType)
+  end
 
-    def getText
-        text.stringValue
-    end
-    
-    def insertText(str)
-        text.setStringValue(str.to_s)
-    end    
+  def windowWillClose(sender)
+    exit
+  end
+
+  private
+
+  def getText
+    text.stringValue
+  end
+
+  def insertText(str)
+    text.setStringValue(str.to_s)
+  end
 end
 
